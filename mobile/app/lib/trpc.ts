a7527c8e5a3fbe19d897252cbed0e106f9e7c109
Ismael <warum7@warum7.com>
2024-10-15 17:59:09 +0200

    feat: add loggerLink

--- mobile/app/lib/trpc.ts
+++ mobile/app/lib/trpc.ts
@@ -1,8 +1,15 @@
-import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import { createTRPCClient, httpBatchLink, loggerLink } from "@trpc/client";
 import type { AppRouter } from "@examples/minimal-trpc";
 
 export const api = createTRPCClient<AppRouter>({
   links: [
+    loggerLink({
+      colorMode: "ansi",
+      enabled: (opts) =>
+        (process.env.NODE_ENV === "development" &&
+          typeof window !== "undefined") ||
+        (opts.direction === "down" && opts.result instanceof Error),
+    }),
     httpBatchLink({
       url: "http://localhost:3000",
     }),
