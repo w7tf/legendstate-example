df8651a95d98e1ceae6f5cd71225f1797b166ac2
Ismael <warum7@warum7.com>
2024-10-17 12:18:47 +0200

    feat: add comment update ui

--- mobile/app/posts/[postId].tsx
+++ mobile/app/posts/[postId].tsx
@@ -1,8 +1,9 @@
 import { ThemedText } from "@/components/ThemedText";
-import { Stack, useLocalSearchParams } from "expo-router";
-import { Button, ScrollView, StyleSheet, View } from "react-native";
-import { store$ } from "../lib/store";
 import { observer } from "@legendapp/state/react";
+import { Link, Stack, useLocalSearchParams } from "expo-router";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { store$ } from "../lib/store";
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 
 function Posts() {
   const { postId } = useLocalSearchParams<{ postId: string }>();
@@ -11,9 +12,9 @@ function Posts() {
     .get()
     .filter((post) => post.id.toString() === postId)[0];
 
-  const comments = store$.comments.filter(
-    (comment) => comment.postId.get() === post.id
-  );
+  const comments = store$.comments
+    .get()
+    .filter((comment) => comment.postId === post.id);
 
   return (
     <ScrollView style={styles.container}>
@@ -26,26 +27,46 @@ function Posts() {
         <ThemedText style={styles.heading}>{post.title}</ThemedText>
         <ThemedText>{post.author}</ThemedText>
         <ThemedText>{post.content}</ThemedText>
-        <ThemedText>
-          Created at: {new Date(post.createdAt).toLocaleString()}
-        </ThemedText>
-        <ThemedText>
-          Updated at: {new Date(post.updatedAt).toLocaleString()}
-        </ThemedText>
+        {post.createdAt && (
+          <ThemedText>
+            Created at: {new Date(post.createdAt).toLocaleString()}
+          </ThemedText>
+        )}
+        {post.updatedAt && (
+          <ThemedText>
+            Updated at: {new Date(post.updatedAt).toLocaleString()}
+          </ThemedText>
+        )}
       </View>
       <View style={styles.commentsContainer}>
-        <ThemedText style={styles.heading}>Comments:</ThemedText>
+        <View style={styles.commentsHeadingContainer}>
+          <ThemedText style={styles.heading}>Comments:</ThemedText>
+          <Link href={`/comments/create/${postId}`} asChild>
+            <TabBarIcon name={"create"} />
+          </Link>
+        </View>
         {comments.map((comment) => (
-          <View key={comment.id.get()}>
-            <ThemedText>
-              {comment.content.get()} ({comment.author.get()})
-            </ThemedText>
-            <ThemedText>
-              Created at: {new Date(comment.createdAt.get()).toLocaleString()}
-            </ThemedText>
-            <ThemedText>
-              Updated at: {new Date(comment.updatedAt.get()).toLocaleString()}
-            </ThemedText>
+          <View key={comment.id} style={styles.commentContainer}>
+            <View>
+              <ThemedText>
+                {comment.content} ({comment.author})
+              </ThemedText>
+              {comment.createdAt && (
+                <ThemedText>
+                  Created at: {new Date(comment.createdAt).toLocaleString()}
+                </ThemedText>
+              )}
+              {comment.updatedAt && (
+                <ThemedText>
+                  Updated at: {new Date(comment.updatedAt).toLocaleString()}
+                </ThemedText>
+              )}
+            </View>
+            <View>
+              <Link href={`/comments/update/${comment.id}`} asChild>
+                <TabBarIcon name={"pencil"} />
+              </Link>
+            </View>
           </View>
         ))}
       </View>
@@ -54,6 +75,20 @@ function Posts() {
 }
 
 const styles = StyleSheet.create({
+  commentsHeadingContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  commentContainer: {
+    padding: 8,
+    justifyContent: "space-between",
+    rowGap: 12,
+    alignContent: "center",
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 4,
+  },
   commentsContainer: {
     paddingVertical: 16,
     gap: 8,
