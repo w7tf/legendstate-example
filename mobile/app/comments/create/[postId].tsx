903659d5350281637e09fe376974852556209d28
Ismael <warum7@warum7.com>
2024-10-17 10:15:22 +0200

    feat: create comments

---
+++ mobile/app/comments/create/[postId].tsx
@@ -0,0 +1,64 @@
+import { observer, useObservable } from "@legendapp/state/react";
+import { randomUUID } from "expo-crypto";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { Button, StyleSheet, TextInput, View } from "react-native";
+import { store$ } from "../../lib/store";
+
+function Comments() {
+  const { postId } = useLocalSearchParams<{ postId: string }>();
+
+  const router = useRouter();
+  const data = useObservable({
+    comment: "",
+    author: "",
+  });
+
+  const handleCreatePost = () => {
+    store$.comments.push({
+      postId: postId,
+      author: data.author.get(),
+      content: data.comment.get(),
+      id: randomUUID(),
+      createdAt: null, // this will be updated by the server
+      updatedAt: null, // this will be updated by the server
+    });
+    router.push("/tutorial");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          title: "Create a comment",
+          headerBackTitleVisible: false,
+        }}
+      />
+      <TextInput
+        onChangeText={(t) => data.comment.set(t)}
+        style={styles.input}
+        placeholder="Comment"
+      />
+      <TextInput
+        onChangeText={(t) => data.author.set(t)}
+        style={styles.input}
+        placeholder="Author"
+      />
+      <Button title="Create Comment" onPress={() => handleCreatePost()} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 6,
+  },
+  input: {
+    padding: 8,
+    borderRadius: 4,
+    margin: 8,
+    borderWidth: 1,
+    borderColor: "gray",
+  },
+});
+export default observer(Comments);
---
+++ server/routes/comments.ts
@@ -0,0 +1,32 @@
+import { gt } from "drizzle-orm";
+import { z } from "zod";
+import { db } from "../db/client";
+import { comments } from "../db/schema";
+import { publicProcedure, router } from "../trpc";
+
+export const commentsRouter = router({
+  list: publicProcedure
+    .input(z.object({ lastSync: z.number().optional() }))
+    .query(async ({ input }) => {
+      const { lastSync } = input;
+
+      return await db
+        .select()
+        .from(comments)
+        .where(lastSync ? gt(comments.updatedAt, lastSync) : undefined);
+    }),
+  create: publicProcedure
+    .input(
+      z.object({
+        content: z.string(),
+        author: z.string(),
+        id: z.string(),
+        postId: z.string(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const values = { ...input, createdAt: undefined, updatedAt: undefined };
+      const data = await db.insert(comments).values(values).returning();
+      return data[0];
+    }),
+});
