edaf422cef77511e4edee01588084bedadd00ae3
Ismael <warum7@warum7.com>
2024-10-17 10:15:31 +0200

    refactor: adjust schema

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -7,26 +7,21 @@ export const posts = sqliteTable("posts", {
   content: text("content").notNull(),
   author: text("author").notNull(),
   updatedAt: integer("updated_at")
-    .notNull()
     .default(sql`(CURRENT_TIMESTAMP)`)
     .$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
   createdAt: integer("created_at")
-    .notNull()
     .default(sql`(CURRENT_TIMESTAMP)`),
 });
 
 export const comments = sqliteTable("comments", {
   id: text("id").primaryKey(),
-  postId: integer("post_id")
+  postId: text("post_id")
     .notNull()
     .references(() => posts.id),
   content: text("content").notNull(),
   author: text("author").notNull(),
   updatedAt: integer("updated_at")
-    .notNull()
     .default(sql`(CURRENT_TIMESTAMP)`)
     .$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
-  createdAt: integer("created_at")
-    .notNull()
-    .default(sql`(CURRENT_TIMESTAMP)`),
+  createdAt: integer("created_at").default(sql`(CURRENT_TIMESTAMP)`),
 });
